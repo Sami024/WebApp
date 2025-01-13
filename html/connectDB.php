f1995dd683c6b6fc3526612afc51c84d0d5eefac
Sami024 <1209181@student.roc-nijmegen.nl>
2025-01-13 19:13:02 +0100

    Update connectDB.php

--- html/connectDB.php
+++ html/connectDB.php
@@ -1,7 +1,7 @@
 <?php
 $dsn = 'mysql:host=localhost;dbname=restaurant';
 $dbusername = 'root';
-$dbpassword = '';
+$dbpassword = 'root';
 
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
