2b5423209ce018aedd7945ec122c410767742e32
Sami024 <1209181@student.roc-nijmegen.nl>
2025-01-25 15:16:36 +0100

    Update V3.8

---
+++ html/contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TwistFood</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+
+    <div class="container">
+        <div class="logo">
+
+            <img src="images/1598562279236_Twist-Food_Logo-1.png"
+                alt="Restaurant Logo" class="img-responsive">
+        </div>
+
+        <ul>
+            <li>
+                <a href="index.html">Home</a>
+            </li>
+            <li>
+                <a href="foods.html">Foods</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Navbar Section Ends Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Sami Benouari</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+
+</html>
